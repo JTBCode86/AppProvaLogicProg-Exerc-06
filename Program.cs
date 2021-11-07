3d495b3047c4c002606dde80a397f1363b37df4f
Jean Barbosa <jeanbarbosa.it@gmail.com>
2021-11-07 18:15:24 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace AppProvaLogicProg_Exerc_06
+{
+    class Program
+    {
+        public static Boolean ValidarSenha(int senha) 
+        {
+            bool flag;
+            try
+            {
+                if (senha==2002)
+                {
+                    flag = true;
+                }
+                else
+                {
+                    flag = false;
+                }
+                return flag;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int senha;
+            bool senhaValida = false;
+            Console.WriteLine("Digite a sua senha: ");
+            senha =int.Parse(Console.ReadLine());
+            senhaValida = ValidarSenha(senha);
+
+            while (!senhaValida)
+            {
+                Console.WriteLine("Senha Invalida");
+                senha = 0;
+                senha = int.Parse(Console.ReadLine());
+                senhaValida = ValidarSenha(senha);
+            }
+
+            Console.WriteLine("Acesso Permitido");
+            Console.ReadLine();
+        }
+    }
+}
